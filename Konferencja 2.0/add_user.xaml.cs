fe13c07f571c7b63651d527eb5040d96edcfed10
Szymon Sobieszczański <sajmek1994@gmail.com>
2017-11-21 22:19:54 +0100

    initialpush

---
+++ Konferencja 2.0/add_user.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Konferencja_2._0
+{
+    /// <summary>
+    /// Interaction logic for add_user.xaml
+    /// </summary>
+    public partial class add_user : Window
+    {
+        public add_user()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
